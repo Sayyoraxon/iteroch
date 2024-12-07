96af37db55456583fa4d2492acacadd14cb3d8d6
Sayyoraxon <sayyoranuriddinova8@gmail.com>
2024-12-08 02:56:41 +0500

    first commit

--- src/hooks/use-outside-click.ts
+++ src/hooks/use-outside-click.ts
@@ -1,20 +1,26 @@
 import React, { useEffect } from "react";
 
+// Define the callback function type
+type OutsideClickCallback = (event: MouseEvent | TouchEvent) => void;
+
 export const useOutsideClick = (
   ref: React.RefObject<HTMLDivElement>,
-  callback: Function
+  callback: OutsideClickCallback // Use the specific callback type here
 ) => {
   useEffect(() => {
-    const listener = (event: any) => {
-      if (!ref.current || ref.current.contains(event.target)) {
+    const listener = (event: MouseEvent | TouchEvent) => {
+      // Ensure event.target is treated as a Node (for contains method)
+      if (!ref.current || ref.current.contains(event.target as Node)) {
         return;
       }
       callback(event);
     };
 
+    // Add event listeners for mousedown and touchstart
     document.addEventListener("mousedown", listener);
     document.addEventListener("touchstart", listener);
 
+    // Cleanup the event listeners when the component unmounts
     return () => {
       document.removeEventListener("mousedown", listener);
       document.removeEventListener("touchstart", listener);
